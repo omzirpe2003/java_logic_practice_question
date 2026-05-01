56a9eb7fca52dff552d72020e5f6c4164b9724b3
omzirpe2006 <omzirpe2006@gmail.com>
2026-05-01 16:24:04 +0530

    Update demo file

--- demo.java
+++ demo.java
@@ -1,7 +1,7 @@
 package java_logic_practice_question;
 class Demo{
     public static void main(String[]args){
-            System.out.println("Demo Bhai");
+            System.out.println("Demo bhai 2 ");
     }
 
 }
