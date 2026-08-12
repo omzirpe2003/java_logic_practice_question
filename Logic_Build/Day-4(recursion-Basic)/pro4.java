52252563a31a5b239e1e24acf8640c59777e1d20
omzirpe2006 <omzirpe2003@gmail.com>
2026-08-12 07:10:47 +0530

    Chnage value

--- Logic_Build/Day-4(recursion-Basic)/pro4.java
+++ Logic_Build/Day-4(recursion-Basic)/pro4.java
@@ -6,6 +6,6 @@ public class pro4 {
     }
     public static void main(String[]args){
         pro4 obj=new pro4();
-        System.out.println(obj.factorial(2));
+        System.out.println(obj.factorial(5));
     }  
 }
